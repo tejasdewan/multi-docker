3782c71a2dabdc002864ba582005fce7cd4c88e0
tejasdewan <tejasdewan@gmail.com>
2020-07-24 16:59:34 +0530

    Custom changes to web page.

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,9 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fib Calci</h1>
             <Link to="/">Home</Link>
+            <br>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
